eb7a0db92c457f620eb7252c95fca39e87874aca
DepsCian <deps2ak@gmail.com>
2025-09-21 20:08:21 +0300

    build: add `tsc-alias` to build script in correct way

--- index.ts
+++ index.ts
@@ -1,13 +1,2 @@
 export * from '@/scraper';
-export { Scraper as _CoreScraper } from '@/core/scraper';
-export * from '@/parsers/auth';
-export * from '@/parsers/comments';
-export * from '@/parsers/continue';
-export * from '@/parsers/genres';
-export * from '@/parsers/movies';
-export * from '@/parsers/person';
-export * from '@/parsers/search';
-export * from '@/parsers/stream';
-export { Movie as MovieParser } from '@/parsers/movie';
-
 export type * from '@/types';
